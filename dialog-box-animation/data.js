97f6c0f4fc8628a6ae65d944a68c9d5e2711e7a2
jlayog <35618625+jlayog@users.noreply.github.com>
2023-06-07 15:05:56 -0400

    Create data.js

---
+++ dialog-box-animation/data.js
@@ -0,0 +1,6 @@
+export const messages = [
+    "Hi, I am Jerel Layog. I am a Frontend Web Developer.",
+    "I love creating software and web apps.",
+    "Looking to hire?",
+    "Check out my work."
+];
